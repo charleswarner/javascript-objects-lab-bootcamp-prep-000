d32a956c437f75887bc919d0fd872f30aa72261d
Charlie Warner <charlie@omnium-events.com>
2018-02-03 03:36:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ object[key] = value
   return object;
 }
 
-/*function deleteFromObjectByKey(object,key) {
+function deleteFromObjectByKey(object,key) {
   delete object.key;
 
   return object;
-} */
+} 
