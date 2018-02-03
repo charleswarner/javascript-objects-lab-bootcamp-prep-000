c20cf1100dd2e5d29fb9d48b5f3046c6afd95a28
Charlie Warner <charlie@omnium-events.com>
2018-02-03 04:29:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 object[key] = value
   return object;
 }
-var newObj = Object.assign({}, object)
 
 function deleteFromObjectByKey(object,key) {
+  var newObj = Object.assign({}, object)
+
+
   delete newObj.key;
 
   return object;
