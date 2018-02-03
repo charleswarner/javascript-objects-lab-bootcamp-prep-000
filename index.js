d39c1c80f50a0f0485a9a74d5c6692f18e0001d9
Charlie Warner <charlie@omnium-events.com>
2018-02-03 03:54:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ object[key] = value
 
 function deleteFromObjectByKey(object,key) {
 delete Object.assign({}, object,key )
-  //delete object.key;
+  delete object.key;
 
   return object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key];
+  delete object[key]
   return object;
 }
