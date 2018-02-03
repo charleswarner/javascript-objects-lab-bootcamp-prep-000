cfda277e4adbbf28712febf7672df00cf3a6c36d
Charlie Warner <charlie@omnium-events.com>
2018-02-03 03:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,12 @@ object[key] = value
 }
 
 function deleteFromObjectByKey(object,key) {
-//delete Object.assign({}, object,key )
   delete object.key;
 
-  return object
+  return object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key];
   return object;
 }
